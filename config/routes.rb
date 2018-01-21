f080df2fdace53777edb737e9cd39ce19b4213f8
Mark Agbuya <markadrianagbuya@gmail.com>
2018-01-21 21:46:11 +1100

    feat(teachers): Show amount of workshops taught

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :attendances
   resources :workshops
   resources :students
+  resources :teachers, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
